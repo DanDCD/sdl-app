13dda13fa12ff03e126a03a7984f4f9f907175c1
DanDCD <daniel.drew2014@outlook.com>
2026-04-25 11:21:34 +0100

    Refactor main application structure to use SDL callbacks and remove window management code

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,32 @@
+#define SDL_MAIN_USE_CALLBACKS 1 // use callbacks for app entrypoints instead of main()
+
+#include "SDL3/SDL_init.h"
+#include "SDL3/SDL_log.h"
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-int main(int argc, char* argv[])
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
-        SDL_Log("SDL_Init failed: %s", SDL_GetError());
-        return 1;
-    }
+    SDL_Log("Hello World!");
 
-    SDL_Window* window = SDL_CreateWindow("Hello SDL3", 800, 600, 0);
-    if (!window) {
-        SDL_Log("SDL_CreateWindow failed: %s", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
+    return SDL_APP_CONTINUE;
+}
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
-    if (!renderer) {
-        SDL_Log("SDL_CreateRenderer failed: %s", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    SDL_Log("Spamming Logs");
 
-    bool running = true;
-    SDL_Event event;
 
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT) {
-                running = false;
-            }
-        }
+    return SDL_APP_CONTINUE;
+}
 
-        SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
-        SDL_RenderClear(renderer);
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    return SDL_APP_CONTINUE;
+}
 
-        SDL_FRect square = { 300.0f, 200.0f, 200.0f, 200.0f };
-        SDL_SetRenderDrawColor(renderer, 255, 100, 50, 255);
-        SDL_RenderFillRect(renderer, &square);
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+    SDL_Log("Quit");
 
-        SDL_RenderPresent(renderer);
-    }
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 0;
 }
